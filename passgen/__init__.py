b35293f838175ee8baf9dd8d539081bfa8928194
jaryaman <j.aryaman25@gmail.com>
2020-12-23 21:59:42 +0000

    Abbreviate punctuation

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -2,19 +2,19 @@ import random
 import string
 
 
-def make_password(n=20, with_punctuation=True):
+def make_password(n=20, punct=True):
     """Make a password
 
     Parameters
     ----------
     n: number of characters
-    with_punctuation: with punctuation characters
+    punct: with punctuation characters
 
     Returns
     -------
     A password generated with the highest quality sources provided by the operating system
     """
-    if with_punctuation:
+    if punct:
         password = ''.join(
             random.SystemRandom().choice(
                 string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation) for _ in range(n)
