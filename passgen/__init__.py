41cb9a937c3e53cf4cadc3f0aa8971c04fec2b83
jaryaman <j.aryaman25@gmail.com>
2020-12-23 21:48:49 +0000

    with punctuation

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -2,8 +2,26 @@ import random
 import string
 
 
-def make_password(n=20):
-    return ''.join(
-        random.SystemRandom().choice(
-            string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(n)
-    )
+def make_password(n=20, with_punctuation=True):
+    """Make a password
+
+    Parameters
+    ----------
+    n: number of characters
+    with_punctuation: with punctuation characters
+
+    Returns
+    -------
+    A password generated with the highest quality sources provided by the operating system
+    """
+    if with_punctuation:
+        password = ''.join(
+            random.SystemRandom().choice(
+                string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation) for _ in range(n)
+        )
+    else:
+        password = ''.join(
+            random.SystemRandom().choice(
+                string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(n)
+        )
+    return password
